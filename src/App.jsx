df48a26472446dc1750d0f6dcd57ce626892a38d
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-01-14 08:13:24 +0000

    feat: add initial file for Groqili project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  const handleSubmit = () => {
+    console.log(content);
+  };
+  return (
+    <main className="flex flex-col min-h-[80vh] justify-center items-center">
+      <h1 className="text-indigo-600 text-4xl"> Groqili | React.js</h1>
+      <p className="text-white mt-4">Ada yang mau tanyakan?</p>
+      <form className="flex flex-col py-4 mt-6 w-80">
+        <input
+          type="text"
+          id="content"
+          placeholder="Ketik pertanyaanmu disini..."
+          className="py-2 px-4 rounded-md text-md text-white bg-gray-800 border border-gray-600 focus:outline-none focus:border-indigo-500"
+        />
+        <button
+          onClick={handleSubmit}
+          id="submit"
+          type="submit"
+          className="bg-indigo-600 text-white py-2 px-4 rounded-r-md hover:bg-indigo-700 mt-5 rounded-md"
+        >
+          Kirim
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default App;

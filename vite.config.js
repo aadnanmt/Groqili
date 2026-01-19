1f1fd62930f32da84d55738b76abbcae76cdcdf4
Adnan <aadnanmtcontact@gmail.com>
2026-01-19 06:16:36 +0000

    feat: initial Project Groqili

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-=======
-
->>>>>>> d6a05ef7732043a498b22f94877020ecd63d01b1
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
